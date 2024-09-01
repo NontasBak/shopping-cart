468c7e1d80a8cb7df43686678e130fbae47653ff
NontasBak <nontasbak@gmail.com>
2024-09-01 23:37:27 +0300

    Add tests for Home component

---
+++ src/components/Home/Home.test.jsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import Home from "./Home";
+import { describe, it, expect } from "vitest";
+import { BrowserRouter } from "react-router-dom";
+
+describe("Home component", () => {
+    it("should render the Home component", () => {
+        render(
+            <BrowserRouter>
+                <Home />)
+            </BrowserRouter>,
+        );
+
+        expect(screen.getByRole("heading").textContent).toMatch("Scrootz");
+        expect(screen.getByText(/best deals/i)).toBeInTheDocument();
+        expect(screen.getByRole("link").textContent).toMatch("Shop now");
+    });
+});
